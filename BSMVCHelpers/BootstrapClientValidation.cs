d34b9cc97e5bf9acb8174436488fd4f457b163cf
Slyusar Pavel <slyusarpavel@gmail.com>
2012-09-28 17:46:30 +0400

    Client Validation update

---
+++ BSMVCHelpers/BootstrapClientValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace Ovixon.Bootstrap
+{
+    public static partial class BootstrapHtmlHelpers
+    {
+        public static MvcHtmlString BootstrapClientValidation(this HtmlHelper htmlHelper)
+        {
+            return new MvcHtmlString(@"
+        function CheckForClientValidation() {
+            var elems = $('.field-validation-valid.text-error');
+            elems.removeClass('text-error');
+            elems.parents('.control-group').removeClass('error');
+
+            elems = $('.field-validation-error').not('.text-error');
+            elems.addClass('text-error');
+            elems.parents('.control-group').addClass('error');
+
+            elems = $('.validation-summary-errors').not('.text-error');
+            elems.addClass('alert').addClass('alert-error');
+
+            setTimeout(CheckForClientValidation, 300);
+        }
+");
+        }
+    }
+}
---
+++ BootStrapMVCHelpers/Download/CheckForClientValidation.js
@@ -0,0 +1,14 @@
+﻿function CheckForClientValidation() {
+    var elems = $('.field-validation-valid.text-error');
+    elems.removeClass('text-error');
+    elems.parents('.control-group').removeClass('error');
+
+    elems = $('.field-validation-error').not('.text-error');
+    elems.addClass('text-error');
+    elems.parents('.control-group').addClass('error');
+
+    elems = $('.validation-summary-errors').not('.text-error');
+    elems.addClass('alert').addClass('alert-error');
+
+    setTimeout(CheckForClientValidation, 300);
+}
